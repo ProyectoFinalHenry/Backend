568bf6f6ba5800ec78efa35b5f8269a65174db92
Franco <fcarreras777@gmail.com>
2023-08-31 17:26:15 -0300

    feature/validacionDeAdmin_cambioAIngles

--- src/controllers/controllerAdmin.js
+++ src/controllers/controllerAdmin.js
@@ -7,13 +7,13 @@ const { User, Role } = sequelize.models;
 
 export const authentication = async ({ email, password }) => {
   const user = await User.findOne({ where: { email } });
-  if (!user) throw new Error("Email o contraseña incorrecta");
+  if (!user) throw new Error("Incorrect email or password");
 
   const isValidPassword = await bcrypt.compare(password, user.password);
-  if (!isValidPassword) throw new Error("Email o contraseña incorrecta");
+  if (!isValidPassword) throw new Error("Incorrect email or password");
 
   const { role } = await Role.findByPk(user.RoleId);
-  if (role === "user") throw new Error("No tienes acceso a este sitio");
+  if (role === "user") throw new Error("You do not have access to this site");
 
   const token = jwt.sign(
     { id: user.id, role: user.RoleId },
@@ -29,18 +29,18 @@ export const getUsers = async () => {
   const users = await User.findAll({
     attributes: { exclude: ["resetToken", "password"] },
   });
-  if (!users.length) throw new Error("No se encontraron usuarios");
+  if (!users.length) throw new Error("No users were found");
 
   return users;
 };
 
 export const changeStatus = async ({ userId }) => {
   const user = await User.findByPk(userId);
-  if (!user) throw new Error("No se encontro al usuario");
+  if (!user) throw new Error("The user was not found");
 
   await user.update({ isActive: !user.isActive });
 
   return {
-    status: "Usuario actualizado",
+    status: "User updated",
   };
 };
