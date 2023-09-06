f7aa3f71e2587f37ca84d6219506f40562e82a4e
Franco <fcarreras777@gmail.com>
2023-09-06 13:21:44 -0300

    validateAdmin

--- src/routes/adminRouter.js
+++ src/routes/adminRouter.js
@@ -1,11 +1,17 @@
 import { Router } from "express";
-import { login, getAllUsers, activeUser, getSales } from "../handlers/handlerAdmin.js";
+import {
+  login,
+  getAllUsers,
+  activeUser,
+  getSales,
+} from "../handlers/handlerAdmin.js";
+import { validateAdmin } from "../middlewares/admin.js";
 const adminRouter = Router();
 
 adminRouter
-    .get("/user", getAllUsers)
-    .get("/data", getSales)
-    .put("/user/status/:userId", activeUser) 
-    .post("/login", login)
+  .get("/user",validateAdmin, getAllUsers)
+  .get("/data",validateAdmin, getSales)
+  .put("/user/status/:userId",validateAdmin, activeUser)
+  .post("/login", login);
 
 export default adminRouter;
